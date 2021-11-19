3917eefb534ec946a89f3c790b6da20360a2b856
Jin <lochjin@gmail.com>
2021-11-19 08:05:16 +0800

    qx support build Secp256k1 public key address by ec-to-pkaddr

--- qx/handle_addr.go
+++ qx/handle_addr.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"github.com/Qitmeer/qng/common/encode/base58"
 	"github.com/Qitmeer/qng/common/hash"
+	"github.com/Qitmeer/qng/core/address"
+	"github.com/Qitmeer/qng/crypto/ecc"
 	"github.com/Qitmeer/qng/params"
 )
 
@@ -83,4 +85,36 @@ func EcPubKeyToAddressSTDO(version []byte, pubkey string) {
 
 	address, _ := base58.QitmeerCheckEncode(h, version[:])
 	fmt.Printf("%s\n", address)
+}
+
+func EcPubKeyToPKAddressSTDO(version string, pubkey string) {
+	data, err := hex.DecodeString(pubkey)
+	if err != nil {
+		ErrExit(err)
+	}
+
+	pubKey, err := ecc.Secp256k1.ParsePubKey(data)
+	if err != nil {
+		ErrExit(err)
+	}
+	var param *params.Params
+	switch version {
+	case "mainnet":
+		param = &params.MainNetParams
+	case "privnet":
+		param = &params.PrivNetParams
+	case "testnet":
+		param = &params.TestNetParams
+	case "mixnet":
+		param = &params.MixNetParams
+	default:
+		param = &params.MainNetParams
+	}
+
+	addr, err := address.NewSecpPubKeyAddress(pubKey.SerializeCompressed(), param)
+	if err != nil {
+		ErrExit(err)
+	}
+
+	fmt.Printf("%s\n", addr.String())
 }
