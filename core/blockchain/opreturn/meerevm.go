7179571daa0fe3ec1d9e399db575ff4f461faf42
Jin <lochjin@gmail.com>
2021-12-20 19:09:54 +0800

    Optimize meerevm

--- core/blockchain/opreturn/meerevm.go
+++ core/blockchain/opreturn/meerevm.go
@@ -7,7 +7,6 @@ import (
 )
 
 type MeerEVM struct {
-	hex string
 }
 
 func (m *MeerEVM) GetType() OPReturnType {
@@ -25,21 +24,23 @@ func (m *MeerEVM) Verify(tx *types.Transaction) error {
 		return fmt.Errorf("Tx output value must zero")
 	}
 	if len(tx.TxOut[0].PkScript) <= 0 {
-		return fmt.Errorf("tx PkScript is empty")
+		return fmt.Errorf("tx output is empty")
+	}
+	if len(tx.TxIn[0].SignScript) <= 0 {
+		return fmt.Errorf("tx input is empty")
 	}
 	return nil
 }
 
 func (m *MeerEVM) Init(ops []txscript.ParsedOpcode) error {
-	if len(ops) < 3 {
+	if len(ops) < 2 {
 		return fmt.Errorf("Illegal %s", m.GetType().Name())
 	}
-	m.hex = string(ops[2].GetData())
 	return nil
 }
 
 func (m *MeerEVM) PKScript() []byte {
-	pks, err := txscript.NewScriptBuilder().AddOp(txscript.OP_RETURN).AddOp(txscript.OP_MEER_EVM).AddData([]byte(m.hex)).Script()
+	pks, err := txscript.NewScriptBuilder().AddOp(txscript.OP_RETURN).AddOp(txscript.OP_MEER_EVM).Script()
 	if err != nil {
 		log.Error(err.Error())
 		return nil
@@ -47,12 +48,9 @@ func (m *MeerEVM) PKScript() []byte {
 	return pks
 }
 
-func (m *MeerEVM) GetHex() string {
-	return m.hex
-}
 
-func NewEVMTx(hex string) *MeerEVM {
-	return &MeerEVM{hex: hex}
+func NewEVMTx() *MeerEVM {
+	return &MeerEVM{}
 }
 
 func IsMeerEVM(pks []byte) bool {
