4fad3dfd6bd2866fba56b3b4965420f97933eadb
Jin <lochjin@gmail.com>
2022-06-21 21:47:02 +0800

    Optimize upgradedb

--- core/blockchain/upgradedb.go
+++ core/blockchain/upgradedb.go
@@ -3,17 +3,19 @@ package blockchain
 import (
 	"fmt"
 	"github.com/Qitmeer/qng/common/roughtime"
+	"github.com/Qitmeer/qng/core/dbnamespace"
 	"github.com/Qitmeer/qng/core/serialization"
 	"github.com/Qitmeer/qng/database"
 )
 
 // update db to new version
 func (b *BlockChain) upgradeDB() error {
-	preVersion := uint32(currentDatabaseVersion - 1)
+	version8 := uint32(8)
+	version9 := uint32(9)
 	if b.dbInfo.version == currentDatabaseVersion {
 		return nil
-	} else if b.dbInfo.version != preVersion {
-		return fmt.Errorf("Only supported update version(%d) -> version(%d), but cur db is version:%d\n", preVersion, currentDatabaseVersion, b.dbInfo.version)
+	} else if b.dbInfo.version != version8 && b.dbInfo.version != version9 {
+		return fmt.Errorf("Only supported update version(%d or %d) -> version(%d), but cur db is version:%d\n", version8, version9, currentDatabaseVersion, b.dbInfo.version)
 	}
 	log.Info(fmt.Sprintf("Update cur db to new version: version(%d) -> version(%d) ...", b.dbInfo.version, currentDatabaseVersion))
 	err := b.indexManager.Drop()
@@ -22,6 +24,23 @@ func (b *BlockChain) upgradeDB() error {
 	}
 	err = b.db.Update(func(dbTx database.Tx) error {
 		bidxStart := roughtime.Now()
+		meta := dbTx.Metadata()
+		serializedData := meta.Get(dbnamespace.ChainStateKeyName)
+		if serializedData == nil {
+			return nil
+		}
+		state, err := DeserializeBestChainState(serializedData)
+		if err != nil {
+			return err
+		}
+
+		if b.dbInfo.version == version8 {
+			err = b.bd.UpgradeDB(dbTx, &state.hash, state.total, b.params.GenesisHash)
+			if err != nil {
+				return err
+			}
+		}
+
 		// save
 		b.dbInfo = &databaseInfo{
 			version: currentDatabaseVersion,
@@ -29,7 +48,7 @@ func (b *BlockChain) upgradeDB() error {
 			bidxVer: currentBlockIndexVersion,
 			created: roughtime.Now(),
 		}
-		err := dbPutDatabaseInfo(dbTx, b.dbInfo)
+		err = dbPutDatabaseInfo(dbTx, b.dbInfo)
 		if err != nil {
 			return err
 		}
