101e2b8597626544c2d6e1f726458796e380f861
James <James@gmail.com>
2022-08-06 10:52:18 +0800

    add base asset

--- testutils/pos/asset.sol
+++ testutils/pos/asset.sol
@@ -2,19 +2,23 @@
 pragma solidity ^0.8.0;
 
 contract Asset {
+
+    address public owner;
+
+    struct MinerInfo {
+        uint256 startTime;
+        bool enable;
+        uint256 rewardPerSec;
+        uint256 lastMiningTime;
+    }
+
     // var
-    uint256 public startTime; // mining start time
-    bool public start; // start time
-
-    mapping(address => uint256) miners; // miners
-
-    uint256 public allPower; // all power
-    uint256 public lastMiningTime; // last mining time
-    uint256 public rewardPerSec; // reward per second
+    mapping(address => MinerInfo) miners; // miners
 
     // events
     event Mining(address indexed _miner, uint256 _value);
-    event SetMiner(address indexed _miner, uint256 _power);
+    event SetMiner(address indexed _miner, MinerInfo mi);
+    event Deposit(address indexed _user, uint256 _value);
 
     constructor() {
         owner = msg.sender;
@@ -26,7 +30,7 @@ contract Asset {
     }
 
     modifier onlyMiner() {
-        require(miners[msg.sender] > 0, 'not miner!!!');
+        require(miners[msg.sender].enable, 'not miner!!!');
         _;
     }
 
@@ -35,37 +39,22 @@ contract Asset {
         emit Deposit(msg.sender, msg.value);
     }
 
-    // set release start time
-    function setStartTime(uint256 _start) external onlyOwner {
-        startTime = _start;
-    }
-
-    function setStart(bool _start) external onlyOwner {
-        start = _start;
-    }
-
-    function setRewardPerSec(bool _reward) external onlyOwner {
-        rewardPerSec = _reward;
-    }
-
-    function setMiner(address miner ,uint256 _power) external onlyOwner {
-        allPower -= miners[miner];
-        miners[miner] = _power;
-        allPower += _power;
-        emit SetMiner(miner,_power);
+    function setMiner(uint256 startTime,bool start,address miner,uint256 _reward) external onlyOwner {
+        miners[miner].enable = start;
+        miners[miner].startTime = startTime;
+        miners[miner].rewardPerSec = _reward;
+        miners[miner].lastMiningTime = block.timestamp;
+        emit SetMiner(miner,miners[miner]);
     }
 
     function mining() external onlyMiner payable{
-        require(start,"not start");
-        require(allPower > 0,"not start");
-        require(block.timestamp >= startTime,"not start");
-        require(block.timestamp >= lastMiningTime,"not start");
-        if(block.timestamp <= lastMiningTime){
+        require(miners[msg.sender].enable,"not start");
+        require(block.timestamp >= miners[msg.sender].startTime,"not start");
+        if(block.timestamp <= miners[msg.sender].lastMiningTime){
             return;
         }
-        uint256 canMining = (block.timestamp - lastMiningTime) * rewardPerSec;
-        lastMiningTime = block.timestamp;
-        canMining = canMining * miners[msg.sender] / allPower;
+        uint256 canMining = (block.timestamp - miners[msg.sender].lastMiningTime) * miners[msg.sender].rewardPerSec;
+        miners[msg.sender].lastMiningTime = block.timestamp;
         if (canMining > payable(address(this)).balance){
             canMining = payable(address(this)).balance;
         }
