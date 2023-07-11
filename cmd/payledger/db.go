b9ef61f6cb0b4e2a2cf4189935cf29d6f965591b
Jin <lochjin@gmail.com>
2023-07-11 12:27:35 +0800

    Optimize payledger

--- cmd/payledger/db.go
+++ cmd/payledger/db.go
@@ -1,12 +1,5 @@
 package main
 
-const (
-	// blockDbNamePrefix is the prefix for the block database name.  The
-	// database type is appended to this value to form the full block
-	// database name.
-	blockDbNamePrefix = "blocks"
-)
-
 var (
 	// DebugAddrInfoBucketName is the name of the db bucket used to house the
 	// debug address info
