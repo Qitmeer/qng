c5924373a490c63a8f2cd424769fce44c51b4392
Jin <lochjin@gmail.com>
2023-12-14 19:50:50 +0800

    mock node support API

--- testutils/simulator/blockchain_test.go
+++ testutils/simulator/blockchain_test.go
@@ -1,24 +1,64 @@
 package simulator
 
 import (
-	"fmt"
+	"encoding/json"
+	"github.com/Qitmeer/qng/config"
+	qjson "github.com/Qitmeer/qng/core/json"
+	"github.com/Qitmeer/qng/core/types/pow"
 	"testing"
-	"time"
 )
 
 func TestMockNode(t *testing.T) {
-	node, err := StartMockNode()
+	node, err := StartMockNode(nil)
 	if err != nil {
 		t.Error(err)
 	}
-	for i := 0; i < 10; i++ {
-		time.Sleep(time.Second)
-		fmt.Println("wait for:", i)
+	defer node.Stop()
+
+	nodeinfo, _ := node.GetPublicBlockChainAPI().GetNodeInfo()
+
+	jsonString, err := json.Marshal(nodeinfo.(*qjson.InfoNodeResult))
+	if err != nil {
+		t.Fatal(err)
 	}
-	defer func() {
-		err = node.Stop()
-		if err != nil {
-			t.Error(err)
-		}
-	}()
+	t.Log(string(jsonString))
+}
+
+func TestGenerateBlocks(t *testing.T) {
+	node, err := StartMockNode(nil)
+	if err != nil {
+		t.Error(err)
+	}
+	defer node.Stop()
+
+	targetBlockNum := uint32(5)
+	ret, err := node.GetPrivateMinerAPI().Generate(targetBlockNum, pow.MEERXKECCAKV1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(ret) != int(targetBlockNum) {
+		t.Fatalf("generate block number error: %d != %d ", len(ret), targetBlockNum)
+	}
+	info, err := node.GetPublicMinerAPI().GetMinerInfo()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(info)
+
+	blockCount, _ := node.GetPublicBlockAPI().GetBlockCount()
+	if blockCount.(uint) != uint(targetBlockNum+1) {
+		t.Fatalf("block count error: %d != %d ", blockCount.(uint), targetBlockNum+1)
+	}
+}
+
+func TestOverrideCfg(t *testing.T) {
+	node, err := StartMockNode(func(cfg *config.Config) error {
+		cfg.DebugLevel = "trace"
+		cfg.DebugPrintOrigins = true
+		return nil
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	defer node.Stop()
 }
--- testutils/simulator/mocknode.go
+++ testutils/simulator/mocknode.go
@@ -3,12 +3,14 @@ package simulator
 import (
 	"github.com/Qitmeer/qng/common/system"
 	"github.com/Qitmeer/qng/config"
+	"github.com/Qitmeer/qng/core/blockchain"
 	_ "github.com/Qitmeer/qng/database/legacydb/ffldb"
 	"github.com/Qitmeer/qng/log"
 	_ "github.com/Qitmeer/qng/meerevm/common"
 	"github.com/Qitmeer/qng/node"
 	"github.com/Qitmeer/qng/params"
 	"github.com/Qitmeer/qng/services/common"
+	"github.com/Qitmeer/qng/services/miner"
 	"github.com/Qitmeer/qng/version"
 	"os"
 	"runtime"
@@ -23,28 +25,34 @@ func DefaultConfig() *config.Config {
 	cfg.DisableRPC = true
 	cfg.DisableListen = true
 	cfg.NoDiscovery = true
-
-	cfg.DebugPrintOrigins = true
-	cfg.DebugLevel = "trace"
+	cfg.Miner = true
 	return cfg
 }
 
-func StartMockNode() (*node.Node, error) {
-	cfg := DefaultConfig()
+var mockNodeGlobalID uint
+
+type MockNode struct {
+	id          uint
+	n           *node.Node
+	wallet      *testWallet
+	overrideCfg func(cfg *config.Config) error
+	//
+	publicMinerAPI      *miner.PublicMinerAPI
+	privateMinerAPI     *miner.PrivateMinerAPI
+	publicBlockAPI      *blockchain.PublicBlockAPI
+	publicBlockChainAPI *node.PublicBlockChainAPI
+}
+
+func (mn *MockNode) Start(cfg *config.Config) error {
 	err := common.SetupConfig(cfg)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	defer func() {
-		if log.LogWrite() != nil {
-			log.LogWrite().Close()
-		}
-	}()
 	interrupt := system.InterruptListener()
 
 	// Show version and home dir at startup.
-	log.Info("System info", "QNG Version", version.String(), "Go version", runtime.Version())
+	log.Info("System info", "QNG Version", version.String(), "Go version", runtime.Version(), "ID", mn.id)
 	log.Info("System info", "Home dir", cfg.HomeDir)
 
 	if cfg.NoFileLogging {
@@ -55,17 +63,85 @@ func StartMockNode() (*node.Node, error) {
 	n, err := node.NewNode(cfg, params.ActiveNetParams.Params, interrupt)
 	if err != nil {
 		log.Error("Unable to start server", "listeners", cfg.Listener, "error", err)
-		return nil, err
+		return err
 	}
+	mn.n = n
 	err = n.RegisterService()
 	if err != nil {
-		return nil, err
+		return err
 	}
 	err = n.Start()
 	if err != nil {
 		log.Error("Uable to start server", "error", err)
 		n.Stop()
+		return err
+	}
+
+	return nil
+}
+
+func (mn *MockNode) Stop() {
+	if log.LogWrite() != nil {
+		log.LogWrite().Close()
+	}
+	if mn.n != nil {
+		err := mn.n.Stop()
+		if err != nil {
+			log.Error(err.Error())
+		}
+	}
+}
+
+func (mn *MockNode) GetPublicMinerAPI() *miner.PublicMinerAPI {
+	if mn.publicMinerAPI == nil {
+		mn.publicMinerAPI = miner.NewPublicMinerAPI(mn.n.GetQitmeerFull().GetMiner())
+	}
+	return mn.publicMinerAPI
+}
+
+func (mn *MockNode) GetPrivateMinerAPI() *miner.PrivateMinerAPI {
+	if mn.privateMinerAPI == nil {
+		mn.privateMinerAPI = miner.NewPrivateMinerAPI(mn.n.GetQitmeerFull().GetMiner())
+	}
+	return mn.privateMinerAPI
+}
+
+func (mn *MockNode) GetPublicBlockAPI() *blockchain.PublicBlockAPI {
+	if mn.publicBlockAPI == nil {
+		mn.publicBlockAPI = blockchain.NewPublicBlockAPI(mn.n.GetQitmeerFull().GetBlockChain())
+	}
+	return mn.publicBlockAPI
+}
+
+func (mn *MockNode) GetPublicBlockChainAPI() *node.PublicBlockChainAPI {
+	if mn.publicBlockChainAPI == nil {
+		mn.publicBlockChainAPI = node.NewPublicBlockChainAPI(mn.n.GetQitmeerFull())
+	}
+	return mn.publicBlockChainAPI
+}
+
+func StartMockNode(overrideCfg func(cfg *config.Config) error) (*MockNode, error) {
+	mn := &MockNode{id: mockNodeGlobalID}
+	cfg := DefaultConfig()
+	if overrideCfg != nil {
+		err := overrideCfg(cfg)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	mockNodeGlobalID++
+	err := mn.Start(cfg)
+	if err != nil {
 		return nil, err
 	}
-	return n, nil
+	wallet, err := newTestWallet(uint32(mn.id))
+	if err != nil {
+		return nil, err
+	}
+	mn.wallet = wallet
+	if len(mn.n.Config.MiningAddrs) <= 0 {
+		mn.n.Config.SetMiningAddrs(wallet.miningAddr())
+	}
+	return mn, nil
 }
