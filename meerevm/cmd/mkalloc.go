2ef1b55f2043f43cfed962991548f9ff6c42fe67
Jin <lochjin@gmail.com>
2024-05-16 09:36:58 +0800

    Optimize mkalloc

--- meerevm/cmd/mkalloc.go
+++ meerevm/cmd/mkalloc.go
@@ -21,32 +21,26 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fileContent := "// It is called by go generate and used to automatically generate pre-computed \n// Copyright 2017-2022 The qitmeer developers \n// This file is auto generate by : go run mkalloc.go \npackage meer\n\n"
+	fileHeader := "// It is called by go generate and used to automatically generate pre-computed \n// Copyright 2017-2022 The qitmeer developers \n// This file is auto generate by : go run mkalloc.go \npackage meer\n\n"
+	fileContent := fileHeader
 	fileContent += fmt.Sprintf("\nconst genesisJson = `%s`", genesisJ)
 	fileContent += fmt.Sprintf("\nconst burnListJson = `%s`", burnListJ)
-
+	err = saveFile("./../meer/genesis_alloc.go", fileContent)
+	if err != nil {
+		panic(err)
+	}
+	fileContent = fileHeader
 	for _, np := range params.AllNetParams {
 		alloc := meer.DoDecodeAlloc(np.Params, genesisJ, burnListJ)
 		genesis := meer.Genesis(np.Net, alloc)
 		genesisHash := genesis.ToBlock().Hash()
 		log.Printf("network = %s, genesisHash= %s\n", np.Name, genesisHash.String())
-		fileContent += fmt.Sprintf("\n\nconst %sGenesisHash = \"%s\"", np.Net.String(), genesisHash.String())
+		fileContent += fmt.Sprintf("\nconst %sGenesisHash = \"%s\"", np.Net.String(), genesisHash.String())
 	}
-
-	fileName := "./../meer/genesis_alloc.go"
-
-	f, err := os.Create(fileName)
-
+	err = saveFile("./../meer/genesis_hash.go", fileContent)
 	if err != nil {
-		panic(fmt.Sprintf("Save error:%s  %s", fileName, err))
+		panic(err)
 	}
-	defer func() {
-		err = f.Close()
-	}()
-
-	f.WriteString(fileContent)
-
-	fmt.Println("Successfully updated:", fileName)
 }
 
 func readFromFile(filePath string) (string, error) {
@@ -60,4 +54,21 @@ func readFromFile(filePath string) (string, error) {
 		return "", err
 	}
 	return buff.String(), nil
+}
+
+func saveFile(fileName string, fileContent string) error {
+	f, err := os.Create(fileName)
+
+	if err != nil {
+		return fmt.Errorf("Save error:%s  %s", fileName, err)
+	}
+	defer func() {
+		err = f.Close()
+	}()
+
+	f.WriteString(fileContent)
+
+	fmt.Println("Successfully updated:", fileName)
+
+	return nil
 }
---
+++ meerevm/meer/genesis_hash.go
@@ -0,0 +1,10 @@
+// It is called by go generate and used to automatically generate pre-computed 
+// Copyright 2017-2022 The qitmeer developers 
+// This file is auto generate by : go run mkalloc.go 
+package meer
+
+
+const MainNetGenesisHash = "0x6cff8746d87573e2667d0784ea8753c7ee1dfc67cdb24971fe5f34743b0208fc"
+const TestNetGenesisHash = "0xf6020a5a812534ea0b5b94b125e1278d79ac7142e9553e3c93c936f0337e4b6a"
+const PrivNetGenesisHash = "0x528b2448923b0d08dc20c692eeb5f42d758e28804c6d341bfb3de9f452290db4"
+const MixNetGenesisHash = "0x9928b53a768f54ec7803c0ae7085cb79ffd526d399970afea82cae54261abfe2"
