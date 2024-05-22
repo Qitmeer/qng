73b8b2c4c1153839d4639ca475e9dfccc3339bc0
Alex Wu <dindinw@users.noreply.github.com>
2024-05-22 14:56:01 +0800

    bump up version to 2.0 for dev/2.0 branch

--- version/version.go
+++ version/version.go
@@ -25,8 +25,8 @@ const (
 // These constants define the application version and follow the semantic
 // versioning 2.0.0 spec (http://semver.org/).
 const (
-	Major uint = 1
-	Minor uint = 2
+	Major uint = 2
+	Minor uint = 0
 	Patch uint = 0
 )
 
