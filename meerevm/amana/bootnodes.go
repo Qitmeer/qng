f6d4e19752937afc3ffdb0aeedcdb568124c459f
Jin <lochjin@gmail.com>
2024-07-19 10:59:12 +0800

    Optimize bootnodes

--- meerevm/amana/bootnodes.go
+++ meerevm/amana/bootnodes.go
@@ -3,23 +3,23 @@ package amana
 // MainnetBootnodes are the enode URLs of the P2P bootstrap nodes running on
 // the main amana network.
 var MainnetBootnodes = []string{
-	"enr:-KO4QIENooHG456eQKBddpyvBsSXOoGwQzCKuyDFuveEwtDtdBW5u8Y8PVD4pW_Q7ZxS8PmO5fsZX41pNPHbutzZ8ZqGAZBH2lGFg2V0aMfGhMM2XVOAgmlkgnY0gmlwhHhPWhiJc2VjcDI1NmsxoQMVziJkqN43CMvklSowHF63Zsd9fgXRXTAiqdHvkvpjG4RzbmFwwIN0Y3CCRtODdWRwgkbT",
+	"",
 }
 
 // TestnetBootnodes are the enode URLs of the P2P bootstrap nodes running on
 // the test amana network.
 var TestnetBootnodes = []string{
-	"enr:-KO4QIENooHG456eQKBddpyvBsSXOoGwQzCKuyDFuveEwtDtdBW5u8Y8PVD4pW_Q7ZxS8PmO5fsZX41pNPHbutzZ8ZqGAZBH2lGFg2V0aMfGhMM2XVOAgmlkgnY0gmlwhHhPWhiJc2VjcDI1NmsxoQMVziJkqN43CMvklSowHF63Zsd9fgXRXTAiqdHvkvpjG4RzbmFwwIN0Y3CCRtODdWRwgkbT",
+	"enr:-KO4QOHfKA2tj93BO7yc70jpCDU-MEkT5p47ySiMpJsqM4Yqe-jUUaCn4wAtt5GjoyWNKmhhuUNYJIMicNbcsp5QaxWGAZDIrj5vg2V0aMfGhDFBn3SAgmlkgnY0gmlwhHhPWhiJc2VjcDI1NmsxoQMVziJkqN43CMvklSowHF63Zsd9fgXRXTAiqdHvkvpjG4RzbmFwwIN0Y3CCRtODdWRwgkbT",
 }
 
 // PrivnetBootnodes are the enode URLs of the P2P bootstrap nodes running on
 // the private amana network.
 var PrivnetBootnodes = []string{
-	"enr:-KO4QIENooHG456eQKBddpyvBsSXOoGwQzCKuyDFuveEwtDtdBW5u8Y8PVD4pW_Q7ZxS8PmO5fsZX41pNPHbutzZ8ZqGAZBH2lGFg2V0aMfGhMM2XVOAgmlkgnY0gmlwhHhPWhiJc2VjcDI1NmsxoQMVziJkqN43CMvklSowHF63Zsd9fgXRXTAiqdHvkvpjG4RzbmFwwIN0Y3CCRtODdWRwgkbT",
+	"",
 }
 
 // MixnetBootnodes are the enode URLs of the P2P bootstrap nodes running on
 // the mix amana network.
 var MixnetBootnodes = []string{
-	"enr:-KO4QIENooHG456eQKBddpyvBsSXOoGwQzCKuyDFuveEwtDtdBW5u8Y8PVD4pW_Q7ZxS8PmO5fsZX41pNPHbutzZ8ZqGAZBH2lGFg2V0aMfGhMM2XVOAgmlkgnY0gmlwhHhPWhiJc2VjcDI1NmsxoQMVziJkqN43CMvklSowHF63Zsd9fgXRXTAiqdHvkvpjG4RzbmFwwIN0Y3CCRtODdWRwgkbT",
+	"enr:-KO4QOHfKA2tj93BO7yc70jpCDU-MEkT5p47ySiMpJsqM4Yqe-jUUaCn4wAtt5GjoyWNKmhhuUNYJIMicNbcsp5QaxWGAZDIrj5vg2V0aMfGhDFBn3SAgmlkgnY0gmlwhHhPWhiJc2VjcDI1NmsxoQMVziJkqN43CMvklSowHF63Zsd9fgXRXTAiqdHvkvpjG4RzbmFwwIN0Y3CCRtODdWRwgkbT",
 }
