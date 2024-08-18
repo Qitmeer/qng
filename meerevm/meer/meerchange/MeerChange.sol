e79cd87c17b8f4acd3b6c27d1cac3f92c2d5cafb
Jin <lochjin@gmail.com>
2024-08-18 09:50:08 +0800

    MeerChange.sol support import operation

--- meerevm/meer/meerchange/MeerChange.sol
+++ meerevm/meer/meerchange/MeerChange.sol
@@ -2,9 +2,14 @@
 pragma solidity ^0.8.0;
 
 contract MeerChange {
+    // Convert to UTXO precision
+    uint256 public constant TO_UTXO_PRECISION = 1e10;
+    // The count of call export
     uint64 private exportCount;
+    // The count of call import
     uint64 private importCount;
 
+    // events
     event Export(
         bytes32 txid,
         uint32 idx
@@ -17,21 +22,41 @@ contract MeerChange {
         string sig
     ); 
 
+    event Import(
+    ); 
+
+    // Export amount from UTXO
     function export(bytes32 txid,uint32 idx) public {
         exportCount++;
         emit Export(txid,idx);
     }
 
+    // Export amount from UTXO by EIP-4337
     function export4337(bytes32 txid,uint32 idx,uint64 fee,string calldata sig) public {
         exportCount++;
         emit Export4337(txid,idx,fee,sig);
     }
 
-    function getExport() public view returns (uint64) {
+    // Get the count of export
+    function getExportCount() public view returns (uint64) {
         return exportCount;
     }
 
-    function getImport() public view returns (uint64) {
+    // Import to UTXO account system
+    function importToUtxo() external payable {
+        uint256 up = msg.value/TO_UTXO_PRECISION;
+        require(up > 0, "To UTXO amount must not be empty");
+        importCount++;
+        emit Import();
+    }
+
+    // Get the count of import
+    function getImportCount() public view returns (uint64) {
         return importCount;
+    }
+
+    // Get the total of import amount
+    function getImportTotal() external view returns (uint256) {
+        return address(this).balance;
     }
 }
