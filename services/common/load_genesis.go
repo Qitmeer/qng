ca07c345e1a9c26ec4f449643e528930e132d754
jin <lochjin@gmail.com>
2026-03-28 21:53:56 +0800

    feat:optimize custom_amana.json

--- services/common/load_genesis.go
+++ services/common/load_genesis.go
@@ -8,7 +8,6 @@ import (
 	"math/big"
 	"os"
 	"path/filepath"
-	"strconv"
 	"time"
 
 	"github.com/Qitmeer/qng/common/util"
@@ -23,18 +22,17 @@ import (
 )
 
 type CustomGenesis struct {
-	Name    string `json:"name"`
-	ChainID uint64 `json:"chain_id"`
-	PoA     struct {
-		Period uint64 `json:"period"`
-		Epoch  uint64 `json:"epoch"`
+	Name string `json:"name"`
+	PoA  struct {
+		Period  uint64   `json:"period"`
+		Epoch   uint64   `json:"epoch"`
+		Signers []string `json:"signers"`
 	} `json:"poa"`
-	TargetTimePerBlock int                 `json:"target_time_per_block"`
-	BaseSubsidy        int64               `json:"base_subsidy"`
-	CoinbaseMaturity   uint16              `json:"coinbase_maturity"`
-	Timestamp          string              `json:"timestamp"`
-	Signers            []string            `json:"signers"`
-	Alloc              etypes.GenesisAlloc `json:"alloc"`
+	EVMConfig struct {
+		ChainID   uint64              `json:"chain_id"`
+		Timestamp int64               `json:"timestamp"`
+		Alloc     etypes.GenesisAlloc `json:"alloc"`
+	} `json:"evm"`
 }
 
 func applyCustomGenesis(cfg *config.Config) error {
@@ -51,21 +49,16 @@ func applyCustomGenesis(cfg *config.Config) error {
 	if err := json.Unmarshal(data, &f); err != nil {
 		return fmt.Errorf("applyCustomGenesis: parse json: %w", err)
 	}
-	if len(f.Signers) == 0 {
+	if len(f.PoA.Signers) == 0 {
 		return fmt.Errorf("applyCustomGenesis: signers must not be empty")
 	}
 
-	signers := make([]common.Address, 0, len(f.Signers))
-	for _, s := range f.Signers {
+	signers := make([]common.Address, 0, len(f.PoA.Signers))
+	for _, s := range f.PoA.Signers {
 		if !common.IsHexAddress(s) {
 			return fmt.Errorf("applyCustomGenesis: invalid signer address %q", s)
 		}
 		signers = append(signers, common.HexToAddress(s))
-	}
-
-	ts, err := strconv.ParseInt(f.Timestamp, 10, 64)
-	if err != nil {
-		return fmt.Errorf("applyCustomGenesis: timestamp: %w", err)
 	}
 
 	poaCfg, ok := params.AmanaNetParams.ConsensusConfig.(*cfgengine.PoAConfig)
@@ -74,8 +67,6 @@ func applyCustomGenesis(cfg *config.Config) error {
 	}
 	if f.PoA.Period > 0 {
 		poaCfg.Period = f.PoA.Period
-	} else if f.TargetTimePerBlock > 0 {
-		poaCfg.Period = uint64(f.TargetTimePerBlock)
 	}
 	if f.PoA.Epoch > 0 {
 		poaCfg.Epoch = f.PoA.Epoch
@@ -84,20 +75,9 @@ func applyCustomGenesis(cfg *config.Config) error {
 	if f.Name != "" {
 		params.AmanaNetParams.Name = f.Name
 	}
-	if f.BaseSubsidy > 0 {
-		params.AmanaNetParams.BaseSubsidy = f.BaseSubsidy
-	}
-	if f.CoinbaseMaturity > 0 {
-		params.AmanaNetParams.CoinbaseMaturity = f.CoinbaseMaturity
-	}
-	if f.TargetTimePerBlock > 0 {
-		tt := time.Second * time.Duration(f.TargetTimePerBlock)
-		params.AmanaNetParams.TargetTimePerBlock = tt
-		params.AmanaNetParams.TargetTimespan = tt * 100
-	}
 
-	if f.ChainID > 0 {
-		params.AmanaNetParams.MeerConfig.ChainID = new(big.Int).SetUint64(f.ChainID)
+	if f.EVMConfig.ChainID > 0 {
+		params.AmanaNetParams.MeerConfig.ChainID = new(big.Int).SetUint64(f.EVMConfig.ChainID)
 		err = eparams.AddMeerChainConfig(&eparams.MeerChainConfig{ChainID: params.AmanaNetParams.MeerConfig.ChainID, Name: f.Name, Type: eparams.Amana})
 		if err != nil {
 			return err
@@ -105,16 +85,16 @@ func applyCustomGenesis(cfg *config.Config) error {
 	}
 
 	gb := params.AmanaNetParams.GenesisBlock.Block()
-	gb.Header.Timestamp = time.Unix(ts, 0)
+	gb.Header.Timestamp = time.Unix(f.EVMConfig.Timestamp, 0)
 	gb.Header.Engine.(*ptypes.PoA).Signers = signers
 
 	params.AmanaNetParams.GenesisBlock = types.NewBlock(gb)
 	params.AmanaNetParams.GenesisHash = params.AmanaNetParams.GenesisBlock.Hash()
 
-	if len(f.Alloc) <= 0 {
+	if len(f.EVMConfig.Alloc) <= 0 {
 		params.AmanaNetParams.MeerAlloc = etypes.GenesisAlloc{}
 	} else {
-		params.AmanaNetParams.MeerAlloc = f.Alloc
+		params.AmanaNetParams.MeerAlloc = f.EVMConfig.Alloc
 	}
 	return nil
 }
